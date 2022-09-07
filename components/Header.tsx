2a989e98e71cd9a751e6d2b10c64dd263a43ce32
jainvandana01 <vandana.jain@nagarro.com>
2022-09-07 13:20:18 +0530

    Dashboard commits

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Header =() => (
+<div className="sticky flex top-0 z-40 w-full h-24 bg-zinc-900">
+    <div className="flex w-full h-full justify-between max-w-7xl px-4">
+<Link href="/">
+<div className="flex items-center cursor-pointer">
+    <Image width="250" height="50" src="/Suzuki-logo.png" alt="suzuki-logo"/>
+</div>
+</Link>
+    </div>
+</div>
+);
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}','./components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    gridTemplateColumns:
+    {
+      'auto-fill': 'repeat(auto-fill, minmax(200px,1fr))'
+    },
+    extend: {},
+    fontFamily:{
+      raleway:["Raleway","Sans-serif"]
+    }
+  },
+  plugins: [],
+}
