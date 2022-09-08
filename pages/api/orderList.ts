4d53456345977195f9a3f6a4952670eae54adcd4
jainvandana01 <vandana.jain@nagarro.com>
2022-09-08 19:44:44 +0530

    add formatting on save

--- pages/api/orderList.ts
+++ pages/api/orderList.ts
@@ -1,14 +1,14 @@
-import type { NextApiRequest, NextApiResponse } from "next"
+import type { NextApiRequest, NextApiResponse } from "next";
 
 //API urls
 //import { BASE_URL } from "../../config"
 
-import { basicFetch } from "../../api/fetchFunction"
+import { basicFetch } from "../../api/fetchFunction";
 
-
-export default async function handler (req:NextApiRequest, res:NextApiResponse) {
-
-    const data = await basicFetch('http://127.0.0.1:3001/');
-    res.status(200).json(data)
-    
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const data = await basicFetch("http://127.0.0.1:3001/");
+  res.status(200).json(data);
 }
